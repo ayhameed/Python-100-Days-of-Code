b83e8f84aa8bbc9575dabb1e75b2f6bd4cd3e6f8
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2022-08-23 15:59:46 +0100

    Day 1 task : Random Band Name generator using user input

---
+++ Day 1/bandnamegenerator.py
@@ -0,0 +1,11 @@
+#Band name generator
+#objective : learn input output and string concatenation
+
+print("Welcome to the Band Name Genrator \n")
+
+city = input("What city did you grow up in? \n")
+
+pet = input("Whats your pets name? \n")
+
+band_name = city + pet
+print("Your Band Name could be: \n" + band_name)
