cf87d95d762f0e28a5a05953cfc862e4f976d579
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2022-09-02 18:55:57 +0100

    banker_roulette.py

---
+++ Day 4/banker_roulette.py
@@ -0,0 +1,18 @@
+import random
+from random import randrange
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+length_name = len(names)
+random_payer_num = randrange(length_name)
+payer = names[random_payer_num]
+
+print(f"{payer} is going to buy the meal today!")
